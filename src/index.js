d329b4510c881d3b64001cca86e3fc3ba18aed16
Aizat <s.aizat21@gmail.com>
2022-07-29 19:03:44 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,17 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let input = expr.match(/.{1,10}/g);
+    
+    const arr = input.toString().split("00");
+    const filtredArr = arr.filter(function (el) {return el != "";});
+    // const morse = filtredArr.toString().replaceAll('11', '-').replaceAll('10', '.').replaceAll('**********', ' ');
+    const dash = filtredArr.toString().split('11').join('-');
+    const dot = dash.split('10').join('.');
+    const space = dot.split('**********').join(' ');
+
+    const solution = space.split(',').map(a => a.split(' ').map(b => MORSE_TABLE[b]).join(' ')).join('');
+    return solution;
 }
 
 module.exports = {
